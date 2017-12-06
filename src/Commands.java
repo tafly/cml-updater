793fcc4024961c568bf7626622655b470f94b1f6
Tomas Frohawk <tom.frohawk@hotmail.co.uk>
2017-12-06 22:07:55 +0000

    Minor changes

--- src/Commands.java
+++ src/Commands.java
@@ -3,31 +3,36 @@ import java.util.Scanner;
 
 public class Commands {
 	void showCommands() {
+		boolean toUpdate = false;
+		String hs = null;
 		String command;
 		Scanner input = new Scanner(System.in);
 
 		System.out
-				.println("What would you like to do? (1=Log XP Update, 2=Get Player Stats, 3=Update CML, q=quit)");
+				.println("What would you like to do? (1=Update Z HS, q=quit)");
 		command = input.next();
 
 		Processing process = new Processing();
 
 		switch (command) {
 		case "1":
-			System.out.println("Which skill?");
+			toUpdate = true;
+			hs = "zplayers.txt";
 
-			process.logSkill(input.next());
 			break;
-		case "2":
-			System.out.println("Which player?");
-
-			process.processURL(input.next());
+		case "q":
+			System.out.println("Program is terminating!");
 			break;
-		case "3":
 
+		default:
+			System.out.println("What's that mean?");
+			break;
+		}
+
+		
+		if (toUpdate = true) {
 			try {
-				BufferedReader br = new BufferedReader(new FileReader(
-						"toplvl3.txt"));
+				BufferedReader br = new BufferedReader(new FileReader(hs));
 
 				for (String line = br.readLine(); line != null; line = br
 						.readLine()) {
@@ -38,16 +43,6 @@ public class Commands {
 			} catch (IOException ex) {
 
 			}
-
-			
-			break;
-		case "q":
-			System.out.println("Program is terminating!");
-			break;
-
-		default:
-			System.out.println("What's that mean?");
-			break;
 		}
 
 		input.close();
--- src/Processing.java
+++ src/Processing.java
@@ -1,73 +1,11 @@
 import java.net.*;
 import java.io.*;
-import java.util.Scanner;
-import java.util.Date;
+
 
 public class Processing {
-	void logSkill(String skill) {
-		String fileName = "xprecords.txt";
-		String xpRecord;
 
-		// String line = null;
-		Date now = new Date();
-
-		Scanner input = new Scanner(System.in);
-
-		Commands commands = new Commands();
-
-		try {
-			FileWriter fileWriter = new FileWriter(fileName, true);
-
-			// Always wrap FileReader in BufferedReader.
-			BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-			// write details to file
-			System.out.println("What is current xp?");
-			xpRecord = (skill + "," + input.next() + "," + now.toString());
-			bufferedWriter.write(xpRecord);
-			bufferedWriter.newLine();
-
-			// Always close files.
-			bufferedWriter.close();
-
-			commands.showCommands();
-
-		} catch (IOException ex) {
-			System.out.println("Error reading file '" + fileName + "'");
-			// Or we could just do this:
-			// ex.printStackTrace();
-		}
-
-		input.close();
-	}
-
-	void processURL(String player) {
-
-		String content = null;
-		URLConnection connection = null;
-		
-		try {
-			connection = new URL("http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + player).openConnection();
-			
-			Scanner scanner = new Scanner(connection.getInputStream());
-			
-			scanner.useDelimiter("\\Z");
-			
-			content = scanner.next();
-			
-			scanner.close();
-			
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		
-		System.out.println(content);
-
-	}
-	
 	void updateCML(String player) {
 
-		
 		try {
 			URL url = new URL("http://www.crystalmathlabs.com/tracker/api.php?type=update&player=" + player);
 			URLConnection yc = url.openConnection();
